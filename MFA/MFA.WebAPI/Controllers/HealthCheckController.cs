7e87f1a0ced4a27944695402bdb2c9a672f1eabc
gustaeps <gustaeps@gmail.com>
2024-01-28 11:10:51 -0300

    Add Endpoint: HealthCheck

---
+++ MFA/MFA.WebAPI/Controllers/HealthCheckController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MFA.WebAPI.Controllers
+{
+    [Route("api/health-check")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public ActionResult HealthCheck()
+        {
+            try
+            {
+                return Ok();
+            } catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
