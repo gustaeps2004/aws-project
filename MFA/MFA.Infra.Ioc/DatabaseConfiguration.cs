b6ba6a67791c387eed9f4ed0cb4d7b6f814afe64
gustaeps <gustaeps@gmail.com>
2024-02-02 23:42:50 -0300

    Adding EF Core Connection

---
+++ MFA/MFA.Infra.Ioc/DatabaseConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using MFA.Infra.Data.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MFA.Infra.Ioc
+{
+    public static class DatabaseConfiguration
+    {
+        public static IServiceCollection AddDatabaseConfiguration(this IServiceCollection services)
+        {
+            services.AddDbContext<SQLServerContextEfCore>(options => 
+            options.UseSqlServer(Environment.GetEnvironmentVariable("SQL_SERVER_CONNECTION_STRING")));
+
+            return services;
+        }
+    }
+}
